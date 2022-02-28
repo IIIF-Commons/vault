2f7577476ea9299959ac601cf0b5261c6a965fe0
Stephen Fraser <stephen.fraser@digirati.com>
2022-02-28 14:24:47 +0000

    Added batch import state

--- src/store/reducers/batch-reducer.ts
+++ src/store/reducers/batch-reducer.ts
@@ -1,4 +1,4 @@
-import { BatchAction, BATCH_ACTIONS } from '../../actions/batch-actions';
+import { BatchAction, BATCH_ACTIONS, BATCH_IMPORT } from '../../actions/batch-actions';
 import { AllActions } from '../../types';
 import { Reducer } from 'redux';
 
@@ -6,6 +6,10 @@ export function createBatchReducer(rootReducer: Reducer<AllActions>) {
   return (state: any, action: BatchAction | AllActions) => {
     if (action && action.type === BATCH_ACTIONS) {
       return action.payload.actions.reduce(rootReducer, state);
+    }
+
+    if (action && action.type === BATCH_IMPORT) {
+      return action.payload.state;
     }
 
     return rootReducer(state, action);
